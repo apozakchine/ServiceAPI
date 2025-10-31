7e86161c54106804db8d80397356f091852c7aa7
Alexander Pozakshin <darkman.russia@gmail.com>
2025-10-31 11:47:30 +0300

    Fix error descriptions

--- Sources/ServiceAPI/Error/Error.swift
+++ Sources/ServiceAPI/Error/Error.swift
@@ -8,36 +8,69 @@
 import Foundation
 
 public enum Error: Swift.Error, LocalizedError {
+    
     case message(String)
+    
     case status(HTTP.Status, Data)
+    
     case system(Swift.Error)
+    
     case encode(DecodingError)
+    
     case custom(any CustomError)
     
-    public var localizedDescription: String? {
+    /// A localized message describing what error occurred.
+    public var errorDescription: String? {
         switch self {
         case let .message(string):
             return string
         case let .status(status, _):
-            return "\("http.status".localized()): \(status.rawValue)"
+            return "HTTP status error: \(status.rawValue)"
         case let .system(error):
             return error.localizedDescription
         case let .encode(error):
             switch error {
-            case let .typeMismatch(_, context):
-                return "\("type.mismatch".localized()): \(context.debugDescription) (\(context.codingPath)"
+            case let .typeMismatch(value, context):
+                var codingPathDescription: String = context
+                    .codingPath
+                    .compactMap({$0.stringValue})
+                    .joined(separator: " -> ")
+                return "\(context.debugDescription) { \(codingPathDescription) }"
+                
             case let .valueNotFound(_, context):
-                return "\("value.not.found".localized()): (\(context.codingPath))"
-            case let .keyNotFound(key, context):
-                return "\("key.not.found".localized()): \(key.description) (\(context.debugDescription)"
+                var codingPathDescription: String = context
+                    .codingPath
+                    .compactMap({$0.stringValue})
+                    .joined(separator: " -> ")
+                return "\(context.debugDescription) { \(codingPathDescription) }"
+                
+            case let .keyNotFound(key, _):
+                return "Key not found: \"\(key.stringValue)\""
+                
             case let .dataCorrupted(context):
                 return context.debugDescription
+                
             default:
                 return error.localizedDescription
             }
         case let .custom(error):
             return error.localizedDescription
         }
+    }
+
+    /// A localized message describing the reason for the failure.
+    public var failureReason: String? {
+        return nil
+    }
+
+    /// A localized message describing how one might recover from the failure.
+    public var recoverySuggestion: String? {
+        return nil
+    }
+
+    /// A localized message providing "help" text if the user requests help.
+    public var helpAnchor: String? {
+        return nil
     }
     
     public var isAuthorizeError: Bool {
@@ -46,4 +79,5 @@ public enum Error: Swift.Error, LocalizedError {
         }
         return status == .unauthorized
     }
+    
 }
